c9fab3a8541f128dc42a0a24dfc6f6bc09cfc042
florinv88 <florinvisan022@gmail.com>
2024-11-12 22:53:09 +0200

    init project

---
+++ postis/src/main/java/com/fnkcode/postis/PostisApplication.java
@@ -0,0 +1,13 @@
+package com.fnkcode.postis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostisApplication.class, args);
+	}
+
+}
---
+++ postis/src/test/java/com/fnkcode/postis/PostisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fnkcode.postis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
